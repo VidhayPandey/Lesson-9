a0b02bff2f223f2a541c01f78becc00ad3260237
VidhayPandey <poojapandey0621@gmail.com>
2026-05-20 12:46:21 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,17 @@
+age= int(input("Enter your Age : "))
+
+if age >= 10: 
+    if age <= 20: 
+        print("Allowed.")
+    else: 
+        print("Not allowed because age is too high.")
+else: 
+    print("Not allowed because age is too low.")
+
+  
+
+  
+   
+
+
+
